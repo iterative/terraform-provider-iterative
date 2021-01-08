b4fa73878fa3f115a26f00446ee363c88c18fe9a
Restyled.io <commits@restyled.io>
2021-01-08 14:29:41 +0000

    Restyled by terraform

--- examples/main.tf
+++ examples/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     iterative = {
       versions = ["0.6"]
-      source = "github.com/iterative/iterative"
+      source   = "github.com/iterative/iterative"
     }
 
     # iterative = {
@@ -16,18 +16,18 @@ provider "iterative" {}
 
 
 resource "iterative_cml_runner" "runner-gl-aws" {
-    name = "gitlabrunner2"
-    token = "arszDpb3xtNdKaXmQ6vN"
-    repo = "https://gitlab.com/DavidGOrtega/3_tensorboard"
-    driver = "gitlab"
-    labels = "tf"
+  name   = "gitlabrunner2"
+  token  = "arszDpb3xtNdKaXmQ6vN"
+  repo   = "https://gitlab.com/DavidGOrtega/3_tensorboard"
+  driver = "gitlab"
+  labels = "tf"
 
-    cloud = "azure"
-    region = "us-west"
-    instance_type = "m"
-    spot = true
-    #spot_price = 0.09
-} 
+  cloud         = "azure"
+  region        = "us-west"
+  instance_type = "m"
+  spot          = true
+  #spot_price = 0.09
+}
 
 # resource "iterative_cml_runner" "runner-gh-aws" {
 #     name = "githubrunner2"
