aa6827182fb9b9cd681af47e51690c9f9072233c
0x2b3bfa0 <0x2b3bfa0+git@googlemail.com>
2021-03-10 17:44:18 +0100

    Update tests to match the new project style

--- iterative/utils/runner_test.go
+++ iterative/utils/runner_test.go
@@ -1,23 +1,25 @@
 package utils
 
-import "testing"
+import (
+	"testing"
 
-func TestPositiveReadinessCheck(t *testing.T) {
-	result := IsReady(`
-        -- Logs begin at Wed 2021-01-20 00:25:37 UTC, end at Fri 2021-02-26 15:37:56 UTC. --
-        Feb 26 15:37:20 ip-172-31-6-188 cml.sh[2203]: {"level":"info","time":"···","repo":"···","status":"ready"}
-    `)
-	if !result {
-		t.Errorf("Positive readiness check")
-	}
-}
+	"github.com/stretchr/testify/assert"
+)
 
-func TestNegativeReadinessCheck(t *testing.T) {
-	result := IsReady(`
-        -- Logs begin at Wed 2021-01-20 00:25:37 UTC, end at Fri 2021-02-26 15:37:56 UTC. --
-        Feb 26 15:37:20 ip-172-31-6-188 cml.sh[2203]: {"level":"info","time":"···","repo":"···"}
-    `)
-	if result {
-		t.Errorf("Negative readiness check")
-	}
+func TestReadinessCheck(t *testing.T) {
+	t.Run("Runner log containing a readiness message should be considered ready", func(t *testing.T) {
+		logString := `
+			-- Logs begin at Wed 2021-01-20 00:25:37 UTC, end at Fri 2021-02-26 15:37:56 UTC. --
+			Feb 26 15:37:20 ip-172-31-6-188 cml.sh[2203]: {"level":"info","time":"···","repo":"···","status":"ready"}
+		`
+		assert.True(t, IsReady(logString))
+	})
+
+	t.Run("Runner log not containing a readiness message should not be considered ready", func(t *testing.T) {
+		logString := `
+			-- Logs begin at Wed 2021-01-20 00:25:37 UTC, end at Fri 2021-02-26 15:37:56 UTC. --
+			Feb 26 15:37:20 ip-172-31-6-188 cml.sh[2203]: {"level":"info","time":"···","repo":"···"}
+		`
+		assert.False(t, IsReady(logString))
+	})
 }
