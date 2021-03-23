ce1b75137a6702fc1d4fbf45ee5ab94175752181
Helio Machado <0x2b3bfa0+git@googlemail.com>
2021-03-23 16:38:22 +0100

    Add more AWS regions

--- iterative/aws/provider.go
+++ iterative/aws/provider.go
@@ -336,7 +336,29 @@ func awsClient(region string) (*ec2.EC2, error) {
 }
 
 //ImageRegions provider available image regions
-var ImageRegions = []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2", "eu-north-1", "eu-west-1", "eu-west-2"}
+var ImageRegions = []string{
+	"us-east-2",
+	"us-east-1",
+	"us-west-1",
+	"us-west-2",
+	"af-south-1",
+	"ap-east-1",
+	"ap-south-1",
+	"ap-northeast-3",
+	"ap-northeast-2",
+	"ap-southeast-1",
+	"ap-southeast-2",
+	"ap-northeast-1",
+	"ca-central-1",
+	"eu-central-1",
+	"eu-west-1",
+	"eu-west-2",
+	"eu-south-1",
+	"eu-west-3",
+	"eu-north-1",
+	"me-south-1",
+	"sa-east-1",
+}
 
 func getRegion(region string) string {
 	instanceRegions := make(map[string]string)
