05aa3a385ea295a6d3707441a7fb6651f2347fc7
davidgortega <g.ortega.david@gmail.com>
2021-07-09 10:56:13 +0200

    Wait improvements

--- iterative/resource_runner.go
+++ iterative/resource_runner.go
@@ -7,6 +7,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net"
 	"os"
 	"strconv"
 	"text/template"
@@ -210,7 +211,17 @@ func resourceRunnerCreate(ctx context.Context, d *schema.ResourceData, m interfa
 	var logError error
 	var logEvents string
 	err = resource.Retry(d.Timeout(schema.TimeoutCreate), func() *resource.RetryError {
-		logEvents, logError = resourceMachineLogs(ctx, d, m)
+		switch cloud := d.Get("cloud").(string); cloud {
+		case "kubernetes":
+			logEvents, logError = resourceMachineLogs(ctx, d, m)
+		default:
+			logEvents, logError = utils.RunCommand("journalctl --unit cml --no-pager",
+				2*time.Second,
+				net.JoinHostPort(d.Get("instance_ip").(string), "22"),
+				"ubuntu",
+				d.Get("ssh_private").(string))
+		}
+
 		log.Printf("[DEBUG] Collected log events: %#v", logEvents)
 		log.Printf("[DEBUG] Connection errors: %#v", logError)
 
