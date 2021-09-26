cf1fab3f56352a7bfa82ca087f972c3c4eff3b5c
Felipe Oliveira <fpo@felipeweb.dev>
2021-09-25 21:01:01 -0300

    script

--- iterative/resource_dvc_run.go
+++ iterative/resource_dvc_run.go
@@ -1,16 +1,22 @@
 package iterative
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"strconv"
 	"terraform-provider-iterative/iterative/utils"
+	"text/template"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"gopkg.in/alessio/shellescape.v1"
 )
 
 func resourceDVCRun() *schema.Resource {
@@ -149,10 +155,6 @@ func resourceDVCRun() *schema.Resource {
 	}
 }
 
-func dvcRunProvisioner(d *schema.ResourceData) (string, error) {
-	return "", nil
-}
-
 func resourceDVCRunCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 
@@ -197,7 +199,7 @@ func resourceDVCRunCreate(ctx context.Context, d *schema.ResourceData, m interfa
 		case "kubernetes":
 			logEvents, logError = resourceMachineLogs(ctx, d, m)
 		default:
-			logEvents, logError = utils.RunCommand("journalctl --unit dvc --no-pager",
+			logEvents, logError = utils.RunCommand("journalctl --unit dvc-run --no-pager",
 				2*time.Second,
 				net.JoinHostPort(d.Get("instance_ip").(string), "22"),
 				"ubuntu",
@@ -231,4 +233,119 @@ func resourceDVCRunCreate(ctx context.Context, d *schema.ResourceData, m interfa
 	}
 
 	return diags
+}
+
+func dvcRunProvisioner(d *schema.ResourceData) (string, error) {
+	data := make(map[string]interface{})
+	data["repo"] = d.Get("repo").(string)
+	data["dvc_ver"] = d.Get("dvc_ver").(string)
+	data["labels"] = d.Get("labels").(string)
+	data["idle_timeout"] = strconv.Itoa(d.Get("idle_timeout").(int))
+	data["name"] = d.Get("name").(string)
+	data["cloud"] = d.Get("cloud").(string)
+	data["startup_script"] = d.Get("startup_script").(string)
+	data["instance_gpu"] = d.Get("instance_gpu").(string)
+	data["single"] = d.Get("single").(bool)
+	data["AWS_SECRET_ACCESS_KEY"] = os.Getenv("AWS_SECRET_ACCESS_KEY")
+	data["AWS_ACCESS_KEY_ID"] = os.Getenv("AWS_ACCESS_KEY_ID")
+	data["AWS_SESSION_TOKEN"] = os.Getenv("AWS_SESSION_TOKEN")
+	data["AZURE_CLIENT_ID"] = os.Getenv("AZURE_CLIENT_ID")
+	data["AZURE_CLIENT_SECRET"] = os.Getenv("AZURE_CLIENT_SECRET")
+	data["AZURE_SUBSCRIPTION_ID"] = os.Getenv("AZURE_SUBSCRIPTION_ID")
+	data["AZURE_TENANT_ID"] = os.Getenv("AZURE_TENANT_ID")
+	data["GOOGLE_APPLICATION_CREDENTIALS_DATA"] = os.Getenv("GOOGLE_APPLICATION_CREDENTIALS_DATA")
+	data["KUBERNETES_CONFIGURATION"] = os.Getenv("KUBERNETES_CONFIGURATION")
+	data["container"] = isContainerAvailable(d.Get("cloud").(string))
+
+	return renderDVCScript(data)
+}
+
+func renderDVCScript(data map[string]interface{}) (string, error) {
+	var script string
+	startup_script, ok := data["startup_script"].(string)
+	if ok {
+		runnerStartupScript, err := base64.StdEncoding.DecodeString(startup_script)
+		if err != nil {
+			return script, err
+		}
+
+		data["runner_startup_script"] = string(runnerStartupScript)
+	}
+
+	tmpl, err := template.New("deploy").Funcs(template.FuncMap{"escape": shellescape.Quote}).Parse(
+		`#!/bin/sh
+{{- if .runner_startup_script}}
+{{.runner_startup_script}}
+{{- end}}
+
+{{- if not .container}}
+sudo tee /usr/bin/dvc_run.sh << 'EOF'
+#!/bin/sh
+{{- end}}
+
+{{- if .cloud}}
+{{- if eq .cloud "aws"}}
+export AWS_SECRET_ACCESS_KEY={{escape .AWS_SECRET_ACCESS_KEY}}
+export AWS_ACCESS_KEY_ID={{escape .AWS_ACCESS_KEY_ID}}
+export AWS_SESSION_TOKEN={{escape .AWS_SESSION_TOKEN}}
+{{- end}}
+{{- if eq .cloud "azure"}}
+export AZURE_CLIENT_ID={{escape .AZURE_CLIENT_ID}}
+export AZURE_CLIENT_SECRET={{escape .AZURE_CLIENT_SECRET}}
+export AZURE_SUBSCRIPTION_ID={{escape .AZURE_SUBSCRIPTION_ID}}
+export AZURE_TENANT_ID={{escape .AZURE_TENANT_ID}}
+{{- end}}
+{{- if eq .cloud "gcp"}}
+export GOOGLE_APPLICATION_CREDENTIALS_DATA={{escape .GOOGLE_APPLICATION_CREDENTIALS_DATA}}
+{{- end}}
+{{- if eq .cloud "kubernetes"}}
+export KUBERNETES_CONFIGURATION={{escape .KUBERNETES_CONFIGURATION}}
+{{- end}}
+{{- end}}
+pip install virtualenv;
+{{- if eq .dvc_ver "latest"}}
+pip install dvc;
+{{else}}
+pip install dvc=={{.dvc_ver}};
+{{- end}}
+
+git clone {{.repo}} repo;
+cd repo;
+virtualenv -p python .env;
+source .env/bin/activate;
+pip install -r requirements.txt;
+dvc exp run;
+
+
+{{- if not .container}}
+EOF
+sudo chmod +x /usr/bin/dvc_run.sh
+
+sudo bash -c 'cat << EOF > /etc/systemd/system/dvc-run.service
+[Unit]
+  After=default.target
+
+[Service]
+  Type=simple
+  ExecStart=/usr/bin/dvc_run.sh
+
+[Install]
+  WantedBy=default.target
+EOF'
+
+{{- if .cloud}}
+sudo systemctl daemon-reload
+sudo systemctl enable dvc-run.service --now
+{{- end}}
+
+{{- end}}
+`)
+	var customDataBuffer bytes.Buffer
+	err = tmpl.Execute(&customDataBuffer, data)
+
+	if err == nil {
+		script = customDataBuffer.String()
+	}
+
+	return script, err
 }
