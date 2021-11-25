8f4e42545c6b8255bdaeaa796e142a709886adb8
Helio Machado <0x2b3bfa0+git@googlemail.com>
2021-11-25 20:52:48 +0000

    Document magic numbers

--- task/common/identifier.go
+++ task/common/identifier.go
@@ -13,17 +13,23 @@ import (
 
 type Identifier string
 
+const (
+	maximumLongLength = 50
+	shortLength = 16
+)
+
 func (i Identifier) Long() string {
 	re := regexp.MustCompile(`(?s)^tpi-([a-z0-9]+(?:[a-z0-9-]*[a-z0-9])?)-([a-z0-9]+)-([a-z0-9]+)$`)
 
-	if match := re.FindStringSubmatch(string(i)); len(match) > 0 && hash(match[1]+match[2], 4) == match[3] {
+	if match := re.FindStringSubmatch(string(i));
+	   len(match) > 0 && hash(match[1]+match[2], shortLength / 2) == match[3] {
 		return match[0]
 	}
 
-	name := normalize(string(i), 30)
-	digest := hash(string(i), 4)
+	name := normalize(string(i), maximumLongLength - shortLength - uint32(len("tpi---")))
+	digest := hash(string(i), shortLength / 2)
 
-	return fmt.Sprintf("tpi-%s-%s-%s", name, digest, hash(name+digest, 4))
+	return fmt.Sprintf("tpi-%s-%s-%s", name, digest, hash(name+digest, shortLength / 2))
 }
 
 func (i Identifier) Short() string {
@@ -33,13 +39,13 @@ func (i Identifier) Short() string {
 
 // hash deterministically generates a Base36 digest of `size`
 // characters using `identifier` as the seed.
-func hash(identifier string, size uint32) string {
+func hash(identifier string, size uint8) string {
 	digest := sha256.Sum256([]byte(identifier))
 	random := uid.NewRandCustom(bytes.NewReader(digest[:]))
 	encoder := uid.NewEncoderBase36()
-	provider := uid.NewProviderCustom(size, random, encoder)
+	provider := uid.NewProviderCustom(32, random, encoder)
 
-	return provider.MustGenerate().String()
+	return provider.MustGenerate().String()[:size]
 }
 
 // normalize normalizes user-provided identifiers by adapting them to
