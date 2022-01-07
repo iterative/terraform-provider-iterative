e023b6a4ccb008b6852ae4576fb5fe9ef89c49ab
0x2b3bfa0 <0x2b3bfa0+git@googlemail.com>
2022-01-07 11:50:01 +0100

    Add human–friendly error messages

--- iterative/resource_task.go
+++ iterative/resource_task.go
@@ -2,6 +2,7 @@ package iterative
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strings"
 	"time"
@@ -146,10 +147,11 @@ func resourceTaskCreate(ctx context.Context, d *schema.ResourceData, m interface
 
 	if err := task.Create(ctx); err != nil {
 		diags = diagnostic(diags, err, diag.Error)
-		if err := task.Delete(ctx); err != nil {
+		if err := task.Delete(ctx); err == nil {
+			diags = diagnostic(diags, errors.New("deleted all the remaining resources"), diag.Warning)
+		} else {
 			diags = diagnostic(diags, err, diag.Error)
 		}
-		return diags
 	}
 
 	d.SetId(task.GetIdentifier(ctx).Long())
