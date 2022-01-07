12d79e1b99da3184e220da09716b1bfd8d1cadb2
Helio Machado <0x2b3bfa0+git@googlemail.com>
2022-01-07 13:50:04 +0100

    Delete task resources when creation fails (#350)
    * Delete task resources when creation fails
    
    * Add human–friendly error messages

--- iterative/resource_task.go
+++ iterative/resource_task.go
@@ -2,6 +2,7 @@ package iterative
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strings"
 	"time"
@@ -145,7 +146,12 @@ func resourceTaskCreate(ctx context.Context, d *schema.ResourceData, m interface
 	}
 
 	if err := task.Create(ctx); err != nil {
-		return diagnostic(diags, err, diag.Error)
+		diags = diagnostic(diags, err, diag.Error)
+		if err := task.Delete(ctx); err == nil {
+			diags = diagnostic(diags, errors.New("deleted all the remaining resources"), diag.Warning)
+		} else {
+			diags = diagnostic(diags, err, diag.Error)
+		}
 	}
 
 	d.SetId(task.GetIdentifier(ctx).Long())
