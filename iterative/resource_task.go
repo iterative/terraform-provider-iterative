b2e8dd9efce50a2a387322dae2af878e6825c5b8
DavidGOrtega <g.ortega.david@gmail.com>
2022-01-08 14:09:32 +0100

    notpicks

--- iterative/resource_task.go
+++ iterative/resource_task.go
@@ -232,7 +232,7 @@ func resourceTaskBuild(ctx context.Context, d *schema.ResourceData, m interface{
 	}
 
 	val := "true"
-	v["TPI_TASK"] = &(val)
+	v["TPI_TASK"] = &val
 	v["CI"] = nil
 	v["CI_*"] = nil
 	v["GITHUB_*"] = nil
