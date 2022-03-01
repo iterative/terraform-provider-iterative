1aa91f19ac8af408cc90fe24ff960c03a1d6c9b2
Helio Machado <0x2b3bfa0+git@googlemail.com>
2022-03-01 11:00:30 +0100

    Update data_source_credentials.go

--- task/gcp/resources/data_source_credentials.go
+++ task/gcp/resources/data_source_credentials.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 
 	"terraform-provider-iterative/task/common"
 	"terraform-provider-iterative/task/gcp/client"
@@ -30,7 +31,7 @@ func (c *Credentials) Read(ctx context.Context) error {
 	if len(c.Client.Credentials.JSON) == 0 {
 		return errors.New("unable to find credentials JSON string")
 	}
-	credentials := string(c.Client.Credentials.JSON)
+	credentials := strings.ReplaceAll(c.Client.Credentials.JSON, "\n", " ")
 
 	connectionString := fmt.Sprintf(
 		":googlecloudstorage,service_account_credentials='%s':%s",
