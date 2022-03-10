c620cbb02b5487626246e02bbce65d6a0d368eba
Helio Machado <0x2b3bfa0+git@googlemail.com>
2022-03-10 01:46:24 +0000

    Remove unused isOutputValid function

--- iterative/resource_task.go
+++ iterative/resource_task.go
@@ -342,17 +342,3 @@ func diagnostic(diags diag.Diagnostics, err error, severity diag.Severity) diag.
 		Summary:  err.Error(),
 	})
 }
-
-func isOutputValid(path string) bool {
-	f, err := os.Open(path)
-	if err != nil {
-		return true
-	}
-	defer f.Close()
-
-	_, err = f.Readdir(1)
-	if err == io.EOF {
-		return true
-	}
-	return false
-}
