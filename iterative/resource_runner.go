ba744ff407583a50e57e0f6f22c4a5236c300abd
Helio Machado <0x2b3bfa0+git@googlemail.com>
2022-03-24 03:17:29 +0100

    Fix running CML inside CML with `pkg` binaries

--- iterative/resource_runner.go
+++ iterative/resource_runner.go
@@ -341,7 +341,7 @@ export KUBERNETES_CONFIGURATION={{escape .KUBERNETES_CONFIGURATION}}
 {{.runner_startup_script}}
 {{- end}}
 
-HOME="$(mktemp -d)" exec $(which cml-runner || echo "cml runner") \
+HOME="$(mktemp -d)" exec $(which cml-runner || echo $(which cml-internal || echo cml) runner) \
   {{if .name}} --name {{escape .name}}{{end}} \
   {{if .labels}} --labels {{escape .labels}}{{end}} \
   {{if .idle_timeout}} --idle-timeout {{escape .idle_timeout}}{{end}} \
