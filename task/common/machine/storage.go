e0a2c2c76bb57a387b312ae7629576357ee5a83e
Helio Machado <0x2b3bfa0+git@googlemail.com>
2022-04-02 01:41:39 +0000

    Fix `rclone` filters when `output` is `.`

--- task/common/machine/storage.go
+++ task/common/machine/storage.go
@@ -98,14 +98,13 @@ func Status(ctx context.Context, remote string, initialStatus common.Status) (co
 }
 
 func Transfer(ctx context.Context, source, destination string, include string) error {
-	include = filepath.Clean(include)
-	if filepath.IsAbs(include) || strings.HasPrefix(include, "../") {
+	if include = filepath.Clean(include); filepath.IsAbs(include) || strings.HasPrefix(include, "../") {
 		return errors.New("storage.output must be inside storage.workdir")
 	}
 
 	rules := []string{
-		"+ /" + include,
-		"+ /" + include + "/**",
+		"+ " + filepath.Clean("/" + include),
+		"+ " + filepath.Clean("/" + include + "/**"),
 		"- **",
 	}
 
