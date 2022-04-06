e7d1446eed7a7383d6503221ed0eb62febb9733a
Helio Machado <0x2b3bfa0+git@googlemail.com>
2022-04-06 04:52:20 +0200

    Update main.go

--- telemetry/main.go
+++ telemetry/main.go
@@ -17,18 +17,4 @@ func main() {
 }
 
 
-// func mayPanic() {
-//     panic("a problem")
-// }
-
-// func main() {
-
-//     defer func() {
-//         if r := recover(); r != nil {
-//             fmt.Println("Recovered. Error:\n", r)
-//         }
-//     }()
-//     mayPanic()
-
-//     fmt.Println("After mayPanic()")
-// }
+// https://pkg.go.dev/github.com/hashicorp/go-retryablehttp
