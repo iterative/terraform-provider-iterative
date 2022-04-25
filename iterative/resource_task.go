79ffd193fe8d2dac752f4f3642c8765500b6dc6e
Helio Machado <0x2b3bfa0+git@googlemail.com>
2022-04-25 11:11:58 +0200

    Bump default disk size on iterative_task resource

--- iterative/resource_task.go
+++ iterative/resource_task.go
@@ -55,7 +55,7 @@ func resourceTask() *schema.Resource {
 				Type:     schema.TypeInt,
 				ForceNew: true,
 				Optional: true,
-				Default:  30,
+				Default:  35,
 			},
 			"spot": {
 				Type:     schema.TypeFloat,
