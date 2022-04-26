d1b02a3ab982d68d38293f7828d016f9fa50f0e2
Helio Machado <0x2b3bfa0+git@googlemail.com>
2022-04-26 17:11:23 +0200

    Fix endpoint

--- iterative/utils/analytics.go
+++ iterative/utils/analytics.go
@@ -28,7 +28,7 @@ import (
 
 const (
 	Timeout  = 5 * time.Second
-	Endpoint = "https://telemetry.cml.dev?ip_policy=strict"
+	Endpoint = "https://telemetry.cml.dev/api/v1/s2s/event?ip_policy=strict"
 	Token    = "s2s.jtyjusrpsww4k9b76rrjri.bl62fbzrb7nd9n6vn5bpqt"
 )
 
