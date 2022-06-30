3396ccc7a4072e0e31a3ed7635119dfaa6859520
DavidGOrtega <g.ortega.david@gmail.com>
2022-06-30 12:12:43 +0200

    optional telemetry (#493)
    * Analytics
    
    * fix code
    
    * updated to specs
    
    * remove println
    
    * remove duplicated schema
    
    * remove unused
    
    * tighter test
    
    * non blocking and events
    
    * Set version in `.goreleaser.yml` (#537)
    
    * Set version in `.goreleaser.yml`
    
    * Remove `-ldflags` from release.yml
    
    * Remove `v` prefix in `Version`
    
    * Test against the hardcoded value
    
    * use desterministic
    
    * platform
    
    * ci
    
    * fix resumed and hide error
    
    * fire and forget
    
    * no care response
    
    * Add panic handling and background analytics
    
    * Restyled by gofmt
    
    * Add ip_policy=strict
    
    * Fix endpoint
    
    * Use `TypeOf(e).String()` instead of `.Name()`
    
    * Ignore context cancellations from Terraform
    
    * Add DO_NOT_TRACK
    
    * Use timeout instead of context
    
    * feedback fixes
    
    * old json appdirs
    
    * log id
    
    * log appdirs new
    
    * CI test
    
    * remove unused
    
    * mocked id
    
    * fix id
    
    * os
    
    * do not track
    
    * fix test
    
    * undo go.mod
    
    * check for TF_BUILD
    
    Co-authored-by: Helio Machado <0x2b3bfa0+git@googlemail.com>
    
    * Restyled by gofmt
    
    * user_id do-not-track
    
    * restore isCI
    
    * nitpick
    
    * ci unknown
    
    * don't write old file in `new.read() == "do-not-track"`
    
    * do-not-track old file
    
    * fix CI-GH-ID
    
    * error handling
    
    * fix tests
    
    * more error handling
    
    * nolint errcheck
    
    Co-authored-by: Helio Machado <0x2b3bfa0+git@googlemail.com>
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Casper da Costa-Luis <casper.dcl@physics.org>

---
+++ iterative/utils/analytics_test.go
@@ -0,0 +1,89 @@
+package utils
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/wessie/appdirs"
+)
+
+func TestVersion(t *testing.T) {
+	assert.Equal(t, Version, "0.0.0")
+}
+
+func TestTerraformVersion(t *testing.T) {
+	ver, _ := TerraformVersion()
+	assert.Equal(t, strings.HasPrefix(ver, "v"), true)
+}
+
+func TestSystemInfo(t *testing.T) {
+	info := SystemInfo()
+	assert.NotNil(t, info["os_name"])
+	assert.NotNil(t, info["platform_version"])
+}
+
+func TestUserId(t *testing.T) {
+	old := appdirs.UserConfigDir("dvc/user_id", "iterative", "", false)
+	new := appdirs.UserConfigDir("iterative/telemetry", "", "", false)
+
+	userId := "00000000-0000-0000-0000-000000000000"
+	data := map[string]interface{}{
+		"user_id": userId,
+	}
+	json, _ := json.MarshalIndent(data, "", " ")
+
+	_ = os.MkdirAll(filepath.Dir(old), 0644)
+	_ = ioutil.WriteFile(old, json, 0644)
+
+	id, _ := UserId()
+	assert.Equal(t, len(id) == 36, true)
+
+	if !IsCI() {
+		assert.Equal(t, userId == id, true)
+
+		_, err := os.Stat(new)
+		assert.Equal(t, !os.IsNotExist(err), true)
+	}
+}
+
+func TestResourceData(t *testing.T) {
+	cloud := "aws"
+	region := "us-west"
+	machine := "xl"
+	disk_size := 30
+	spot := 0.0
+	status := map[string]interface{}{
+		"running": 0,
+		"failed":  0,
+	}
+	logs := make([]interface{}, 0)
+	logs = append(logs, "2022-04-24 20:25:07 Started tpi-task.service.\n2022-04-24 20:26:07 hello.\n")
+	logs = append(logs, "2022-04-24 20:27:07 Started tpi-task.service.\n")
+
+	d := generateSchemaData(t, map[string]interface{}{
+		"cloud":     cloud,
+		"region":    region,
+		"machine":   machine,
+		"disk_size": disk_size,
+		"spot":      spot,
+		"status":    status,
+		"logs":      logs,
+	})
+
+	data := ResourceData(d)
+
+	assert.Equal(t, cloud, data["cloud"].(string))
+	assert.Equal(t, region, data["cloud_region"].(string))
+	assert.Equal(t, machine, data["cloud_machine"].(string))
+	assert.Equal(t, disk_size, data["cloud_disk_size"].(int))
+	assert.Equal(t, spot, data["cloud_spot"].(float64))
+	assert.Equal(t, true, data["cloud_spot_auto"].(bool))
+	assert.Equal(t, status, data["task_status"].(map[string]interface{}))
+	assert.Equal(t, 120.0, data["task_duration"].(float64))
+	assert.Equal(t, true, data["task_resumed"].(bool))
+}
