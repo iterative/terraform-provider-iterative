07e4116a3ff87fdb6ba69aeb224b723fa1e5840f
Domas Monkus <domas@monkus.lt>
2022-09-05 11:21:19 +0300

    Review fix: remove unused interface.

--- task/common/resource.go
+++ task/common/resource.go
@@ -4,14 +4,6 @@ import (
 	"context"
 )
 
-// BlobContainer defines the interface implemented by blob container resources
-// across cloud providers.
-type BlobContainer interface {
-	Resource
-	// Identifier returns the identifier of the container.
-	Identifier() string
-}
-
 // Resource defines the interface implemented by deployment resources.
 type Resource interface {
 	Read(ctx context.Context) error
