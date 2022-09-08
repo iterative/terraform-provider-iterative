faed28f08f6461c4a2a473357cc43778db67da7d
Helio Machado <0x2b3bfa0+git@googlemail.com>
2022-09-08 02:44:59 +0000

    Log identifier in the beginning

--- cmd/create/create.go
+++ cmd/create/create.go
@@ -120,7 +120,8 @@ func (o *Options) Run(cmd *cobra.Command, args []string, cloud *common.Cloud) er
 		return err
 	}
 
-	fmt.Println(id.Long())
+	logrus.Infof("Using identifier %s", id.Long())
+	defer fmt.Println(id.Long())
 
 	if err := tsk.Create(ctx); err != nil {
 		logrus.Errorf("Failed to create a new task: %v", err)
