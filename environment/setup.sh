c574e4a538fd12037db3005fa3db996be09380ce
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-10-08 19:31:22 +0200

    Update nvidia container support package (#688)
    * Removed deprecated package and installed nvidia-container-toolkit
    
    * Removing apt-get in favor of apt
    
    * Updated golden tests
    
    Co-authored-by: Helio Machado <0x2b3bfa0+git@googlemail.com>

--- environment/setup.sh
+++ environment/setup.sh
@@ -37,7 +37,7 @@ if [ ! -f "$FILE" ]; then
 
     curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
     curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu18.04/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-    sudo apt update && sudo apt install -y nvidia-docker2
+    sudo apt update && sudo apt install -y nvidia-container-toolkit
     sudo systemctl restart docker
   fi
 
