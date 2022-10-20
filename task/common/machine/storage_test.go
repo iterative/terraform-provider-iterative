83ca367e3425ece5b55b1cfedacfd6c07d0c5b5d
Domas Monkus <domas@monkus.lt>
2022-10-20 16:51:28 +0300

    Tests for transfer excludes.

---
+++ task/common/machine/storage_test.go
@@ -0,0 +1,70 @@
+package machine_test
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"strings"
+	"terraform-provider-iterative/task/common/machine"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransferExcludes(t *testing.T) {
+	tests := []struct {
+		description string
+		exclude     []string
+		expect      []string
+	}{{
+		description: "Test builtin rules to exclude terraform files.",
+		exclude:     nil,
+		expect: []string{
+			"/a.txt",
+			"/temp",
+			"/temp/a.txt",
+			"/temp/b.txt",
+		},
+	}, {
+		description: "Test excluding using glob patterns.",
+		exclude:     []string{"*.txt"},
+		expect: []string{
+			"/temp", // directory still gets transfered.
+		},
+	}, {
+		description: "Test rooted excludes.",
+		exclude:     []string{"/temp/a.txt"},
+		expect: []string{
+			"/a.txt",
+			"/temp",
+			"/temp/b.txt",
+		},
+	}}
+	ctx := context.Background()
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			dst := t.TempDir()
+			err := machine.Transfer(ctx, "./testdata/transferTest", dst, test.exclude)
+			require.NoError(t, err)
+			require.ElementsMatch(t, test.expect, listDir(dst))
+		})
+	}
+}
+
+func listDir(dir string) []string {
+	var entries []string
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if path == dir {
+			return nil
+		}
+		entries = append(entries, strings.TrimPrefix(path, dir))
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	return entries
+}
