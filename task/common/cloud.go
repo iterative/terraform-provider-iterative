15b0ae2396f0fc5e4fd76a67fc6be39ccc3865fc
Helio Machado <0x2b3bfa0+git@googlemail.com>
2022-11-18 13:50:29 +0200

    Add structure for passing hardcoded credentials to task.
    Co-authored-by: Domas Monkus <domas@iterative.ai>

--- task/common/cloud.go
+++ task/common/cloud.go
@@ -6,10 +6,11 @@ import (
 )
 
 type Cloud struct {
-	Timeouts Timeouts
-	Provider Provider
-	Region   Region
-	Tags     map[string]string
+	Timeouts    Timeouts
+	Provider    Provider
+	Credentials Credentials
+	Region      Region
+	Tags        map[string]string
 }
 
 type Timeouts struct {
@@ -28,6 +29,34 @@ const (
 	ProviderAZ  Provider = "az"
 	ProviderK8S Provider = "k8s"
 )
+
+type Credentials struct {
+	AWSCredentials *AWSCredentials
+	GCPCredentials *GCPCredentials
+	AZCredentials  *AZCredentials
+	K8SCredentials *K8SCredentials
+}
+
+type AWSCredentials struct {
+	AccessKeyID     string // AWS_ACCESS_KEY_ID
+	SecretAccessKey string // AWS_SECRET_ACCESS_KEY
+	SessionToken    string // AWS_SESSION_TOKEN
+}
+
+type GCPCredentials struct {
+	ApplicationCredentials string // GOOGLE_APPLICATION_CREDENTIALS (contents of file)
+}
+
+type AZCredentials struct {
+	ClientID       string // AZURE_CLIENT_ID
+	ClientSecret   string // AZURE_CLIENT_SECRET
+	SubscriptionID string // AZURE_SUBSCRIPTION_ID
+	TenantID       string // AZURE_TENANT_ID
+}
+
+type K8SCredentials struct {
+	Config string // KUBECONFIG (contents of file)
+}
 
 func (c *Cloud) GetClosestRegion(regions map[string]Region) (string, error) {
 	for key, value := range regions {
