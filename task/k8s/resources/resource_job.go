2e5449c8c4a2c1cd7fea830aa9eae5a7bd7ef1e2
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2023-04-17 15:20:17 +0200

    Revert changes to task

--- task/k8s/resources/resource_job.go
+++ task/k8s/resources/resource_job.go
@@ -99,6 +99,11 @@ func (j *Job) Create(ctx context.Context) error {
 		return common.NotFoundError
 	}
 
+	jobNodeSelector := map[string]string{}
+	for selector, value := range j.Attributes.NodeSelector {
+		jobNodeSelector[selector] = value
+	}
+
 	// Define the accelerator settings (i.e. GPU type, model, ...)
 	jobAccelerator := match[3]
 	jobGPUType := "nvidia.com/gpu"
@@ -111,16 +116,12 @@ func (j *Job) Create(ctx context.Context) error {
 	if diskAmount := j.Attributes.Task.Size.Storage; diskAmount > 0 {
 		jobLimits[kubernetes_core.ResourceEphemeralStorage] = kubernetes_resource.MustParse(strconv.Itoa(diskAmount) + "G")
 	}
-	if jobGPUCount > "0" && jobGPUType != "" {
-		jobLimits[kubernetes_core.ResourceName(jobGPUType)] = kubernetes_resource.MustParse(jobGPUCount)
-	}
 
-	jobNodeSelector := map[string]string{}
-	for selector, value := range j.Attributes.NodeSelector {
-		if value != "infer" {
-			jobNodeSelector[selector] = value
-		} else if jobGPUCount > "0" && jobAccelerator != "" {
-			jobNodeSelector[selector] = jobAccelerator
+	// If the resource requires GPU provisioning, determine how many GPUs and the kind of GPU it needs.
+	if jobGPUCount > "0" {
+		jobLimits[kubernetes_core.ResourceName(jobGPUType)] = kubernetes_resource.MustParse(jobGPUCount)
+		if jobAccelerator != "" {
+			jobNodeSelector["accelerator"] = jobAccelerator
 		}
 	}
 
